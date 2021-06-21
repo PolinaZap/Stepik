dae8c82178f6a4ef354da8aabb631115d856661c
polina <zatula.polina@gmail.com>
2021-06-21 18:50:04 +0300

    programs 7-10

---
+++ program7/main7.py
@@ -0,0 +1,7 @@
+x = int(input('Print any number:'))
+
+if x <= 0:
+    print('This number is False!')
+
+if x > 0:
+    print('This number is True!')
---
+++ program9/main9.py
@@ -0,0 +1,2 @@
+x = int(input('Enter any number:'))
+print(x >= 10 and x < 100)
