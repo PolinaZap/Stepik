838f107a225c182d5671e720177f5068c9f0f810
polina <zatula.polina@gmail.com>
2021-06-22 16:00:27 +0300

    programs 11-15

---
+++ program13/main13.py
@@ -0,0 +1,9 @@
+# сравнение чисел
+a = int(input('Enter the first number:'))
+b = int(input('Enter the second number:'))
+if a >= b:
+    print('The first number is greater.')
+elif a <= b:
+    print('The second number is greater.')
+elif a == b:
+    print('Both numbers are equal.')
